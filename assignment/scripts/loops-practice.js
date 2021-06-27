7139072e9d6d9cfca7f2063bf6f4605bdcc544f4
Dylan Howard <dylanhoward0424@outlook.com>
2021-06-27 03:11:24 -0500

    added while loop counting from 0 to 5

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -57,7 +57,11 @@ while (index<stars.length) {
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
-
+index = 0;
+while (index<6) {
+  console.log(index);
+  index++
+}
 
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
